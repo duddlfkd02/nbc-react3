f02f8d05d924bd59d09611ea1a76da6043fe3aac
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-09-05 08:38:20 +0900

    1st: (1) 비동기 프로그래밍 입문

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // const [message, setMessage] = useState("타이머시작");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  // useEffect(() => {
+  //   const delay = (ms) => {
+  //     const promise = new Promise((resolve) => {
+  //       setTimeout(resolve, ms);
+  //     });
+  //     return promise;
+  //   };
+  //   delay(2000).then(() => {
+  //     setMessage("2초 후 메시지 변경 완료");
+  //   });
+  // }, []);
 
-export default App
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
+
+  return <div>{/* <h1>{message}</h1> */}</div>;
+};
+
+export default App;
